d3be97b7df8eb51cd9069f584880fd66c6612e7f
Jovan <pady.soft@gmail.com>
2018-02-22 19:40:02 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- tenebot/tenebot/Program.cs
+++ tenebot/tenebot/Program.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using tenebot.Services;
 
 namespace tenebot
 {
@@ -50,26 +51,7 @@ namespace tenebot
 
         private Task Log(LogMessage message)
         {
-            switch (message.Severity)
-            {
-                case LogSeverity.Critical:
-                case LogSeverity.Error:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-                case LogSeverity.Warning:
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    break;
-                case LogSeverity.Info:
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
-                case LogSeverity.Verbose:
-                case LogSeverity.Debug:
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    break;
-            }
-            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}");
-            Console.ResetColor();
-
+            Debugging.Log(message);
             return Task.CompletedTask;
         }
 
---
+++ tenebot/tenebot/Services/Settings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Discord;
+
+namespace tenebot.Services
+{
+    public class SettingsMiddleMan
+    {
+        public string ClientId { get; set; }
+        public string BotToken { get; set; }
+        public IList<string> OwnerIds { get; set; }
+    }
+
+    // IM STILL WORKING ON THIS DO NOT TOUCH
+
+    public static class Settings
+    {
+        private static string clientId;
+        private static string botToken;
+        private static IList<string> ownerIds;
+
+        /// <summary>
+        /// Returns the client ID as string
+        /// </summary>
+        public static string ClientId { get => clientId; }
+        /// <summary>
+        /// Returns the bot token as string
+        /// </summary>
+        public static string BotToken { get => botToken; }
+        /// <summary>
+        /// Returns a list of owner ids as strings
+        /// </summary>
+        public static IList<string> OwnerIds { get => ownerIds; }
+
+        /// <summary>
+        /// Loads the settings from a json file and stores it in the settings class variable
+        /// </summary>
+        /// <returns>Returns bool if it loaded or failed to load</returns>
+        public static bool Load()
+        {
+            try
+            {
+                SettingsMiddleMan middleMan = JsonConvert.DeserializeObject<SettingsMiddleMan>("locationoffile");
+
+                clientId = middleMan.ClientId;
+                botToken = middleMan.BotToken;
+                ownerIds = middleMan.OwnerIds;
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debugging.Log(new LogMessage(LogSeverity.Error, "Settings, Load()", $"Exception while trying to load settings from configuration", e));
+
+                return false;
+            }
+        }
+    }
+}
