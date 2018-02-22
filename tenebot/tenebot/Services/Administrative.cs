14ab763137353ef98a4e30857f115cfb1cc52d44
Unknown <xtenebix@gmail.com>
2018-02-22 22:27:43 +0000

    start of services/administrative [wip]

---
+++ tenebot/tenebot/Services/Administrative.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using tenebot.Services;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace tenebot.Services
+{
+    public static class Administrative
+    {
+        
+        public static bool CheckForAdministrator(ulong userId)
+        {
+
+            return true;
+
+        }
+
+    }
+}
