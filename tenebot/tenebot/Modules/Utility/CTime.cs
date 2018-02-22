8c0359f2a1504635b9b6483dd15de09acbe648c9
tomacatx1 <tomacatx1@gmail.com>
2018-02-23 01:54:43 +0200

    added a !time command
    returns current time in UTC format
    added a start meme for !daily (event timer) command
    pretty much me learning discord.net tbh

---
+++ tenebot/tenebot/Modules/Utility/CTime.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+
+namespace tenebot.Modules.Utility
+{
+    public class WhatTime :  ModuleBase<SocketCommandContext>
+    {
+
+        [Command("time")]
+        public async Task outputTime()
+        {
+            DateTime nowtime = DateTime.UtcNow;
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.WithTitle($"{Context.User.Username}, the current time in UTC is : " );
+            embed.Description = nowtime.ToShortTimeString();
+
+        }
+    }
+}
