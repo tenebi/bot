0237f4ae57cfb77bc27cc75da12f49cdabdb1ba7
tomacatx1 <tomacatx1@gmail.com>
2018-02-23 01:19:33 +0200

    !weeb bugfix, part III : the attack of the char arrays
    memes
    changed logic somewhat in what the command displays based on input

--- tenebot/tenebot/Modules/Reactions/Weeb.cs
+++ tenebot/tenebot/Modules/Reactions/Weeb.cs
@@ -12,20 +12,23 @@ namespace tenebot.Modules.Reactions
     public class weeb : ModuleBase<SocketCommandContext>
     {
         Random rand = new Random();
+        string input;
         string[] links = {  "https://www.jovanzlatanovic.com:2083/cpsess3686105827/viewer/home%2fjovanzl%2fpublic_html%2ftenebi%2ftoms_bullshit/1.jpg",
                             "https://www.jovanzlatanovic.com:2083/cpsess3686105827/viewer/home%2fjovanzl%2fpublic_html%2ftenebi%2ftoms_bullshit/2.jpg",
                             "https://www.jovanzlatanovic.com:2083/cpsess3686105827/viewer/home%2fjovanzl%2fpublic_html%2ftenebi%2ftoms_bullshit/3.jpg" };
         [Command("weeb")]
-        public async Task weebT(SocketGuildUser user = null)
+        public async Task weebT(string user)
         {
-
             EmbedBuilder builder = new EmbedBuilder();
-            builder.WithTitle("fucking weebs")
-                .WithDescription($"{Context.User.Mention} is being a dirty weeaboo")
-                .WithImageUrl(links[rand.Next(links.Length)])
-                .WithColor(Color.Orange);
+            if (user.Contains("@") == false)
+            {
+                builder.WithTitle("fucking weebs")
+                    .WithDescription($"{Context.User.Mention} is being a dirty weeaboo")
+                    .WithImageUrl(links[rand.Next(links.Length)])
+                    .WithColor(Color.Orange);
+            }
 
-            if (user == null)
+            else if (user == null)
             {
                 await ReplyAsync("", false, builder.Build());
 
