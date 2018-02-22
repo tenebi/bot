4277b0a86ad6d79dc70746db637adcb4b6bf5a21
Jovan <pady.soft@gmail.com>
2018-02-22 17:50:56 +0100

    more testing with git

--- tenebot/tenebot/Modules/Gambling/roll.cs
+++ tenebot/tenebot/Modules/Gambling/roll.cs
@@ -12,15 +12,35 @@ namespace tenebot.Modules.Gambling
     {
         Random rand = new Random();
         string numbers = "";
+        char[] num = new char[9]; 
         [Command("roll")]
         public async Task roll()
         {
 
-            for (int i = 1; i <= 10; i++)
+            for (int i = 0; i < 9; i++)
             {
 
                 int randint = rand.Next(9);
-                numbers += randint.ToString();
+                num[i] = Convert.ToChar(randint);
+                //numbers += randint.ToString();
+            }
+
+            Array.Reverse(num);
+            char p = num[0];
+            int count = 0;
+
+            foreach (char c in num)
+            {
+                if(c == p)
+                {
+                    count++;
+                }
+
+                else
+                {
+                    break;
+                }
+                p = c;
             }
 
             EmbedBuilder builder = new EmbedBuilder();
@@ -29,17 +49,38 @@ namespace tenebot.Modules.Gambling
                 .WithDescription($"{Context.User.Mention} rolls **{numbers}**")
                 .WithImageUrl("http://i0.kym-cdn.com/entries/icons/original/000/001/714/americanpsycho.jpg");
 
-            if (numbers[9] == numbers[8])
-                builder.Title = "D U B S!";
-            else if (numbers[9] == numbers[8] && numbers[8] == numbers[7])
-                builder.Title = "T R I P S!";
-            else if (numbers[9] == numbers[8] && numbers[8] == numbers[7] && numbers[7] == numbers[6])
-                builder.Title = "Q U A D S!";
-            else if (numbers[9] == numbers[8] && numbers[8] == numbers[7] && numbers[7] == numbers[6] && numbers[6] == numbers[5])
-                builder.Title = "Q  U  I  N  T  S!";
-
+            switch (count)
+            {
+                case 0 :
+                    builder.Title = "singles :(";
+                    break;
+                case 1 :
+                    builder.Title = "D U B S!";
+                    break;
+                case 2 :
+                    builder.Title = "*T R I P S!*";
+                    break;
+                case 3 :
+                    builder.Title = "**Q U A D S!**";
+                    break;
+                case 4 :
+                    builder.Title = "***Q  U  I  N  T  S!***";
+                    break;
+            }
 
             await ReplyAsync("", false, builder.Build());
+
+            /* else if (count == 1)
+                 builder.Title = "D U B S!";
+             else if (count == 2)
+                 builder.Title = "T R I P S!";
+             else if (count == 3)
+                 builder.Title = "Q U A D S!";
+             else if (count == 4)
+                 builder.Title = "Q  U  I  N  T  S!";*/
+
+
+
 
 
         }
