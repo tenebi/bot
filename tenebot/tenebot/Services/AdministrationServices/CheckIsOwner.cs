79d23edc496891f7b29f528ee44eb8379fdcb494
tenebi <xtenebix@gmail.com>
2018-02-23 17:33:39 +0000

    some changes to how administration is handled
    changed CheckIsOwner to take a socketuser, [use context.user to check if the user who sent the message is an owner], began adding some minor administration commands, wip.

---
+++ tenebot/tenebot/Services/AdministrationServices/CheckIsOwner.cs
@@ -0,0 +1,27 @@
+﻿using Discord.WebSocket;
+using System;
+using tenebot.Services;
+
+namespace tenebot.Services.AdministrationServices
+{
+    public static class CheckIsOwner
+    {
+        /// <summary>
+        /// Checks if the id is an OwnerId in the configuration.json.
+        /// </summary>
+        /// <param name="user">SocketGuildUser, use Context.User for this.</param>
+        /// <returns>If the user is an owner.</returns>
+        public static bool check(SocketUser user)
+        {
+            foreach (string ownerid in Settings.OwnerIds)
+            {
+                if (user.Id.ToString() == ownerid)
+                    return true;
+            }
+            
+            Debugging.Log($"ADMINISTRATION", $"Administrator command called by user with insufficient permissions: {user.Username} . If this command call was by you or an administrator, place your client's ID in configuration.json.", Discord.LogSeverity.Warning);
+            return false;
+
+        }
+    }
+}
