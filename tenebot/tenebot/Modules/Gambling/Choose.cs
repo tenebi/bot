f01bc38e4689fbd82f19958b1ed65e48c65fdef4
Jovan <pady.soft@gmail.com>
2018-02-23 19:03:19 +0100

    Choose command
    Chooses shit seperated by comma, i don't think it works properly.

---
+++ tenebot/tenebot/Modules/Gambling/Choose.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace tenebot.Modules.Gambling
+{
+    public class Choose : ModuleBase<SocketCommandContext>
+    {
+        Random rand = new Random();
+        [Command("choose")]
+        public async Task ChoiceFunction(string args)
+        {
+            List<string> seperated = args.Split(',').ToList();
+
+            Random rnd = new Random();
+            int selection = rnd.Next(seperated.Count);
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.WithTitle($"I have chosen: {seperated[selection]}");
+
+            await ReplyAsync("", false, embed.Build());
+        }
+    }
+}
