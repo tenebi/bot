273a6e7fac68e554cdee3997442ffd4bc9ac1e12
Jovan <pady.soft@gmail.com>
2018-02-24 22:42:46 +0100

    Just made a database and connection string, does nothing yet

---
+++ tenebot/tenebot/Services/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tenebot.Services
+{
+    public static class Database
+    {
+        private static string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\TenebotDatabase.mdf;Integrated Security=True";
+
+        public static void TestDatabase()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string command = "INSERT INTO Users(UserId, Username) VALUES ('1234567893', 'test user')";
+
+                using (SqlCommand sqlcommand = new SqlCommand(command, connection))
+                {
+                    sqlcommand.ExecuteNonQuery();
+
+                    Debugging.Log("Database testing", "shit worked", Discord.LogSeverity.Debug);
+                }
+            }
+        }
+    }
+}
