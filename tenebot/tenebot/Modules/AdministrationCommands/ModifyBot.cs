074d60dc86166533c7284f5d6fb7e75b8f7718af
tenebi <xtenebix@gmail.com>
2018-02-26 17:58:07 +0000

    modify command to modify username, pretty useless : )

---
+++ tenebot/tenebot/Modules/AdministrationCommands/ModifyBot.cs
@@ -0,0 +1,29 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using tenebot.Services.AdministrationServices;
+using System.Threading.Tasks;
+
+namespace tenebot.Modules.AdministrationCommands
+{   
+    [Group("modify")]
+    public class ModifyBot : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("username")]
+        public async Task modifyUsername([Remainder] string username)
+        {
+            bool isOwner = Services.AdministrationServices.CheckIsOwner.Check(Context.User);
+            if(isOwner)
+            { 
+                await Context.Client.CurrentUser.ModifyAsync(c => c.Username = username);
+            }
+            else
+            {
+                await ReplyAsync("", false, Embeds.notOwner.Build());
+            }
+        }
+    }
+}
