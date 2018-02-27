03074668f3f5970fd3489e9b7f3d6b6cbcf1eceb
tenebi <xtenebix@gmail.com>
2018-02-27 00:32:26 +0000

    riotapi implementation, some unfinished commands DONT TOUCCH SHIT

--- tenebot/tenebot/Modules/AdministrationCommands/ModifyBot.cs
+++ tenebot/tenebot/Modules/AdministrationCommands/ModifyBot.cs
@@ -5,19 +5,22 @@ using System.Linq;
 using System.Text;
 using tenebot.Services.AdministrationServices;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
 
 namespace tenebot.Modules.AdministrationCommands
-{   
+{
     [Group("modify")]
     public class ModifyBot : ModuleBase<SocketCommandContext>
     {
 
+
         [Command("username")]
         public async Task modifyUsername([Remainder] string username)
         {
-            bool isOwner = Services.AdministrationServices.CheckIsOwner.Check(Context.User);
-            if(isOwner)
-            { 
+            bool isOwner = CheckIsOwner.Check(Context.User);
+            if (isOwner)
+            {
                 await Context.Client.CurrentUser.ModifyAsync(c => c.Username = username);
             }
             else
